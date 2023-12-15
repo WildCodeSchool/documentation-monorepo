4b55b03da4c7d869ef6c45822b87975263d79088
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-12-15 23:05:39 +0100

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
 	organizationName: "Anthony Gorski", // Usually your GitHub org/user name.
 	projectName: "JS-Template", // Usually your repo name.
 
-	onBrokenLinks: "throw",
+	onBrokenLinks: "ignore",
 	onBrokenMarkdownLinks: "warn",
 
 	// Even if you don't use internationalization, you can use this field to set
