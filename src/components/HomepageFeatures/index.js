d9e9c229393bf820cd47fd3b4eda13fbc1fe2f70
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-05-02 17:10:20 +0200

    add link to documentation

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,63 +3,64 @@ import Heading from "@theme/Heading";
 import styles from "./styles.module.css";
 
 const FeatureList = [
-  {
-    title: "Prise en main du Frontend",
-    Svg: require("@site/static/img/undraw_front.svg").default,
-    description: (
-      <>
-        Explorez la conception du frontend avec Harmonia, un framework utilisant
-        React coté frontend, et découvrez comment l'utiliser de manière
-        appropriée.
-      </>
-    ),
-  },
-  {
-    title: "Liaison Frontend/Backend",
-    Svg: require("@site/static/img/undraw_api.svg").default,
-    description: (
-      <>
-        Facilitez la communication entre les parties Frontend et Backend de
-        notre application Harmonia en utilisant une API REST.
-      </>
-    ),
-  },
-  {
-    title: "Prise en main Backend",
-    Svg: require("@site/static/img/undraw_back.svg").default,
-    description: (
-      <>
-        Explorez la conception du backend avec Harmonia, basé sur le micro
-        framework Express, et découvrez comment l'utiliser de manière adéquate.
-      </>
-    ),
-  },
+	{
+		title: "Prise en main du Client",
+		Svg: require("@site/static/img/undraw_front.svg").default,
+		description: (
+			<>
+				Explorez la conception du frontend avec Harmonia, un framework
+				utilisant React coté frontend, et découvrez comment l'utiliser
+				de manière appropriée.
+			</>
+		),
+	},
+	{
+		title: "Utilisation de l'API",
+		Svg: require("@site/static/img/undraw_api.svg").default,
+		description: (
+			<>
+				Facilitez la communication entre les parties Client & Server de
+				notre application Harmonia en utilisant une API REST.
+			</>
+		),
+	},
+	{
+		title: "Prise en main Server",
+		Svg: require("@site/static/img/undraw_back.svg").default,
+		description: (
+			<>
+				Explorez la conception du server avec Harmonia, basé sur le
+				micro framework Express, et découvrez comment l'utiliser de
+				manière adéquate.
+			</>
+		),
+	},
 ];
 
 function Feature({ Svg, title, description }) {
-  return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={clsx("col col--4")}>
+			<div className="text--center">
+				<Svg className={styles.featureSvg} role="img" />
+			</div>
+			<div className="text--center padding-horiz--md">
+				<Heading as="h3">{title}</Heading>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,48 @@
-import clsx from "clsx";
+import { useColorMode } from "@docusaurus/theme-common";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import clsx from "clsx";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import Heading from "@theme/Heading";
 import styles from "./index.module.css";
-
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx("hero hero--primary", styles.heroBanner)}>
+			<div className="container">
+				<Heading as="h1" className="hero__title">
+					{siteConfig.title}
+				</Heading>
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
+				<div>
+					<a
+						href="docs/architecture-monorepo"
+						className={clsx(
+							useColorMode().colorMode === "light"
+								? styles.getStartedLight
+								: styles.getStartedDark
+						)}
+					>
+						Accéder à la documentation
+					</a>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Débuter avec le framework js de la wild code school"
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<Layout
+			title={`${siteConfig.title}`}
+			description="Débuter avec le framework js de la wild code school"
+		>
+			<HomepageHeader />
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	);
 }
